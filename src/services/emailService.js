9b3b88327c545caab95fa316f350bb26d295fc2b
Ubuntu <ubuntu@ip-172-31-2-119.us-east-2.compute.internal>
2025-07-02 23:34:57 +0000

    Correcciones

--- src/services/emailService.js
+++ src/services/emailService.js
@@ -21,7 +21,7 @@ export const sendVerificationEmail = async (email, token) => {
         <div style="font-family: Inter, sans-serif; background-color: #f9f9f9; padding: 32px; color: #111;">
           <div style="max-width: 600px; margin: 0 auto; background: #ffffff; border-radius: 12px; padding: 24px; box-shadow: 0 5px 15px rgba(0,0,0,0.05);">
             <div style="text-align: center;">
-              <img src="https://cdn.emprenet.work/Favicons/NetFavicon.svg" alt="EmpreNet Logo" width="250px" style="margin-bottom: 24px;" />
+              <img src="https://cdn.emprenet.work/Icons/png/Favicon_EmNet.png" alt="EmpreNet Logo" width="250px" style="margin-bottom: 24px;" />
               <h2 style="margin: 0; font-size: 22px; color: #000;">Verifica tu correo electrónico</h2>
               <p style="margin: 16px 0 24px; font-size: 15px; color: #444;">
                 Gracias por registrarte en <strong>EmpreNet</strong>. Para completar tu registro, haz clic en el siguiente botón:
@@ -41,15 +41,16 @@ export const sendVerificationEmail = async (email, token) => {
               <p>Explora más:</p>
               <div style="display: flex; justify-content: center; gap: 16px; flex-wrap: wrap; margin: 16px 0;">
                 <a href="https://emprenet.work" target="_blank" style="text-decoration: none; color: #111; display: inline-flex; align-items: center; gap: 6px;">
-                  <img src="https://cdn.emprenet.work/Favicons/NetFavicon.svg" alt="EmpreNet" width="18px" height="18px" />
+                  <img src="https://cdn.emprenet.work/Icons/png/Favicon_EmNet.png" alt="EmpreNet" width="18px" height="18px" />
                   Oficial Site
                 </a>
                 <a href="https://github.com/Pokaymon/emprenet-api" target="_blank" style="text-decoration: none; color: #111; display: inline-flex; align-items: center; gap: 6px;">
-                  <img src="https://cdn.emprenet.work/Icons/github-logo.svg" alt="GitHub" width="18px" height="18px" />
+                  <img src="https://cdn.emprenet.work/Icons/png/github-logo.png" alt="GitHub" width="18px" height="18px" />
                   @Emprenet.git
                 </a>
                 <a href="https://unidadestecno-my.sharepoint.com/:f:/g/personal/jasocha_uts_edu_co/EhTaGE0X9udOlcGW2Ke_ih0BsoenjeXxP31VEgy3CkkGxw?e=R8M9t" target="_blank" style="text-decoration: none; color: #111; display: inline-flex; align-items: center; gap: 6px;">
-                  Documents
+                  <img src="https://cdn.emprenet.work/Icons/png/github-logo.png" alt="GitHub" width="18px" height="18px" />
+		  Documents
                 </a>
               </div>
               <p style="font-size: 12px; color: #888; margin-top: 24px;">
