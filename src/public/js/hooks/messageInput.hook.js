b26f40bedd0e3028c4e6b99523c0bf10dd141460
Ubuntu <ubuntu@ip-172-31-2-119.us-east-2.compute.internal>
2025-09-26 14:49:15 +0000

    now input and sendBtn are in <form> to use preventDefault();

--- src/public/js/hooks/messageInput.hook.js
+++ src/public/js/hooks/messageInput.hook.js
@@ -1,22 +1,30 @@
+import { appendMessage } from "../renders/messages.render.js";
+
 export function initMessageInput(socket, userId, els) {
-  const input = els.chatConversation.querySelector("input[type='text']");
-  const sendBtn = els.chatConversation.querySelector("button");
+  const form = els.chatConversation?.querySelector("[data-role='chat-form']");
+  const input = els.chatConversation.querySelector("[data-role='chat-input']");
+
+  if (!form || !input) return;
 
   function sendMessage(to, content) {
     socket.emit("private_message", { to, content });
   }
 
-  sendBtn.onclick = () => {
+  // Limpiar listeners previos
+  form.onsubmit = null;
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault(); // Evitar ocultar chatConversation
+
     const content = input.value.trim();
     if (!content) return;
-    sendMessage(userId, content);
-    input.value = "";
-  };
 
-  input.onkeypress = (e) => {
-    if (e.key === "Enter") {
-      e.preventDefault();
-      sendBtn.click();
-    }
-  };
+    // Enviar al backend
+    sendMessage(userId, content);
+
+    // Render inmediato
+    appendMessage({ content }, els, "sent");
+
+    input.value = "";
+  });
 }
